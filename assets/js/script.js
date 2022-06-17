53fd513654c8ef70aa0b92efc5f03e2934df8c8b
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-16 21:57:08 -0700

    added both fetches and added some other minor detials to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,32 @@
 var weatherKey = "3c71aebffae52dc748aa0c2b474d8d34";
-//var city = "San Diego";
 var limit = 1;
-//var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${weatherKey}`;
-var weatherData = ["temp", "humidity", "wind_speed", "uvi"];
-var tempKey = "temp";
-var humidityKey = "humidity";
-
-https://api.openweathermap.org/data/2.5/onecall?lat=33.6856969&lon=-117.8259819&appid=3c71aebffae52dc748aa0c2b474d8d34
-
-
-//https://api.openweathermap.org/geo/1.0/direct?q=irvine&limit=5&appid=3c71aebffae52dc748aa0c2b474d8d34
-
-//"lat":33.6856969,"lon":-117.8259819
-
-
-function getLatLongCity(city) {
-    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${weatherKey}`;
-    fetch (url)
-    .then(function(response) {
-        return response.json();
-    });
-}
-
-//var result = getLatLongCity("Irvine");
-//console.log(result);
 
 
 
-async function getLatLongForCity(city) {
-    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${weatherKey}`;
-    var response = await fetch(url);
-    var data = await response.json();
-    return 
-    //return {lat: data["lat"], lon: data["lon"]};
-}
 
-var latLon = await getLatLongForCity("irvine");
-console.log(latLon);
+  var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${weatherKey}`;
+  var response = await fetch(url);
+  var cityData = await response.json();
+  var cityLat = cityData[0]["lat"];
+  var cityLon = cityData[0]["lon"];
+  
+  document.getElementById("forecast-main-container").innerHTML = "";
+  await populateCurrentDayWeather(city, cityLat, cityLon);
+  await populateForecast(city, cityLat, cityLon);
+  console.log(localStorage);
+  
+
+
+
+
+  var urlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${weatherKey}`;
+  var response2 = await fetch(urlWeather);
+  var data = await response2.json();
+  var currentDay = moment().format("MM/DD/YYYY");
+  $("#current-city").text(city + " (" + currentDay + ")");
+  $("#current-temp").text("Temp: " + kelvinToF(data["current"]["temp"]) + " F");
+  $("#current-wind").text("Wind: " + data["current"]["wind_speed"] + " MPH");
+  $("#current-humidity").text("Humidty: " + data["current"]["humidity"] + " %");
+  $("#current-uv").text("UV Index: " + data["current"]["uvi"]);
+
 
