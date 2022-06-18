8fcaebfeda5983b94e6fe853b61b8d86ecfdb5ac
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-06-18 10:50:23 -0700

    added styling and minor issues with date and formating for javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,24 @@ localStorage.clear();
 
 
 var searchBtn = document.getElementById("search-button");
-searchBtn.addEventListener("click", async function (event) {
+searchBtn.addEventListener("click", async function () {
   var city = $("#city-input").val();
   if (city == null || city.trim() === "")
     return;
   var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${weatherKey}`;
   var response = await fetch(url);
+ 
   var cityData = await response.json();
+  if (cityData == null || cityData.length == 0)
+    return;
+  console.log(cityData);
   var cityLat = cityData[0]["lat"];
   var cityLon = cityData[0]["lon"];
   
   document.getElementById("forecast-main-container").innerHTML = "";
   await populateCurrentDayWeather(city, cityLat, cityLon);
   await populateForecast(city, cityLat, cityLon);
-  console.log(localStorage);
+  console.log(localStorage); 
   
   if (!localStorage.hasOwnProperty(city.trim().toLowerCase())) {
     var localStrVal = { lat: cityLat, lon: cityLon };
@@ -33,6 +37,7 @@ searchBtn.addEventListener("click", async function (event) {
   
 });
 
+
 async function searchHistBtnClicked(event) {
     var city = event.target.textContent;
     city = city.trim().toLowerCase();
@@ -46,6 +51,7 @@ async function populateCurrentDayWeather(city, cityLat, cityLon) {
   var urlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${weatherKey}`;
   var response2 = await fetch(urlWeather);
   var data = await response2.json();
+  $(".current-city-container").css("border", "2px solid black");
   var currentDay = moment().format("MM/DD/YYYY");
   $("#current-city").text(city + " (" + currentDay + ")");
   $("#current-temp").text("Temp: " + kelvinToF(data["current"]["temp"]) + " F");
@@ -60,6 +66,7 @@ async function populateForecast(city, cityLat, cityLon) {
   var data = await response2.json();
   var currentDay = moment().format("MM/DD/YYYY");
   var forecasts = data["daily"];
+  $(".forecast-title").css("display", "block");
   for (var i = 0; i < 5; i++) {
     var forecast = forecasts[i];
     var container = document.createElement("div");
@@ -76,7 +83,8 @@ async function populateForecast(city, cityLat, cityLon) {
     wind.setAttribute("class", "fr-wind");
     humidity.setAttribute("class", "fr-humidity");
 
-    $(dt).text(city + " (" + currentDay + ")");
+    var nextDay = moment().add(i + 1, "d").format("MM/DD/YYYY");
+    $(dt).text("(" + nextDay + ")");
     var imgSrc = forecast["weather"][0]["icon"];
     $(img).attr("src", "https://openweathermap.org/img/w/" + imgSrc + ".png");
     $(temp).text(kelvinToF(forecast["temp"]["day"]) + " F");
